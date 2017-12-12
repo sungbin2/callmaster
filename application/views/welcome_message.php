085c0389f59b023c1134b6f591ecc2a919b6a367
sungbin2 <sungbin222@naver.com>
2017-12-12 13:37:13 +0900

    1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,24 +1,22 @@
 <div class="row">
 	<div class="col-md-8">
 		<div class="jumbotron">
-			<h1>콜마스터 API</h1>
-			<p>모바일 기기에 최적화된 콜마스터 API를 경험하세요.</p>
-			<p><a class="btn btn-primary btn-lg" href="http://sungbin2.iptime.org" role="button">시작하기</a></p>
+			<h1>Seulamat Troek!</h1>
+			<p>Nyoë nakeuh saboh aplikasi masôk ureung lam admin atau ureung biasa</p>
+			<p><a class="btn btn-primary btn-lg" href="http://kautube.com" role="button">Baca lom</a></p>
 		</div>
 	</div>
 	<div class="col-md-4">
 		<div class="list-group">
 			<a href="#" class="list-group-item active">
-				콜마스터 메뉴
+				Vertical Menu
 			</a>
-			<a href="<?php echo site_url() ?>/auth/login" class="list-group-item">로그인</a>
-			<a href="<?php echo site_url() ?>/auth/register" class="list-group-item">회원등록</a>
-<!--
-			<a href="http://kautube.com" class="list-group-item">홈페이지</a>
+			<a href="<?php echo site_url() ?>/auth/login" class="list-group-item">Login Area</a>
+			<a href="<?php echo site_url() ?>/auth/register" class="list-group-item">Register Area</a>
+			<a href="http://kautube.com" class="list-group-item">Kautube.com</a>
 			<a href="https://www.voakita.com" class="list-group-item">voakita.com</a>
 			<a href="http://kurs.kautube.com" class="list-group-item">Kurs Rupiah Konverter</a>
 			<a href="http://android.kautube.com" class="list-group-item">Android Kautube</a>
--->
 		</div>
 	</div>
 </div>
